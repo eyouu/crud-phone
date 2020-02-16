267e0b2b3b9660bcd36c775d23acaf09ba034cd2
Zhovnir Vasyl <vazhovnir@gmail.com>
2020-02-16 16:21:42 +0200

    first commit

---
+++ src/main/java/org/springbootproject/crudphone/CrudPhoneApplication.java
@@ -0,0 +1,13 @@
+package org.springbootproject.crudphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springbootproject/crudphone/CrudappPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbootproject.crudphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudappPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
